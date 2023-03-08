202e55fd4d86aa3b79b489c6e5793a352cf25a4f
jadhav-kunal <jadhavkunal1999@gmail.com>
2023-03-09 01:21:29 +0530

    Added initial styling, data, states, pause/play, time and slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import React, { useState } from "react";
+// Adding components
 import Player from "./components/Player";
 import Song from "./components/Song";
+// Importing styles
+import "./styles/app.scss";
+// Importing util
+import data from "./util";
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <h1>Music Player</h1>
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
