ff3f89d10abf7752df3228c329fe3a834dbad1fa
jadhav-kunal <jadhavkunal1999@gmail.com>
2023-03-08 23:22:37 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Player from "./components/Player";
+import Song from "./components/Song";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Music Player</h1>
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
