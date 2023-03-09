dd1419144801e4aded37c7252f69952db4b60002
jadhav-kunal <jadhavkunal1999@gmail.com>
2023-03-10 02:22:20 +0530

    Added data

--- src/data.js
+++ src/data.js
@@ -3,32 +3,82 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
     {
+      name: "Falling Out of Orbit",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
+      artist: "Knowmadic",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=51080",
+      color: ["#aa8f4c", "#214e39"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Saved For Sunset",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/10/d2fc5032585b1d577f2bd59d5dba4766d99884f6-1024x1024.jpg",
+      artist: "Allem Iversom, little blue",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=42243",
+      color: ["#f9f9f9", "#463640"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Cabriolet",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2023/02/492202568b0d18312224043686735f7f1a47f67b-1024x1024.jpg",
+      artist: "Plusma, Ben Bada Boom",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=49369",
+      color: ["#42ecf5", "#c1c71c"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "What If I Told You",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/07/935da7886600df5eeb2d3b13b12cf27ee8c6c700-1024x1024.jpg",
+      artist: "Juan Rios",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=21649",
+      color: ["#fdf3c9", "#fff4ca"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "La Zona",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/10/3ff29a35be582c8dc0222273cb9c401ea6b209dc-1024x1024.jpg",
+      artist: "Maydee",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=27455",
+      color: ["#b2c2d9", "#f9f9f9"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
       name: "Summer Vibe",
       cover:
         "https://chillhop.com/wp-content/uploads/2023/02/71374d07888a8b1b3fbef53b82d283f53209cc1a-1024x1024.jpg",
-      artist: "Kunal",
+      artist: "Evil Needle X Misha",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=51088",
-      color: ["#205950", "#FFFFFF"],
+      color: ["#6c6558", "#0f0d09"],
       id: uuidv4(),
-      active: true,
+      active: false,
     },
     {
-      name: "In the Night",
+      name: "Delicate",
       cover:
-        "https://chillhop.com/wp-content/uploads/2023/02/492202568b0d18312224043686735f7f1a47f67b-1024x1024.jpg",
-      artist: "Kunal 1",
+        "https://chillhop.com/wp-content/uploads/2023/02/ef337196272ee56d2e76383d39ed446b57472aec-1024x1024.jpg",
+      artist: "Middle School, Henry Gritton",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=51086",
-      color: ["#205950", "#FFFFFF"],
+      color: ["#a18d46", "#514f1f"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Twilight",
       cover:
-        "https://chillhop.com/wp-content/uploads/2023/02/71374d07888a8b1b3fbef53b82d283f53209cc1a-1024x1024.jpg",
-      artist: "Kunal 2",
+        "https://chillhop.com/wp-content/uploads/2022/12/b48463b9b5a0351cc2f34b31fcee6185fdb03bb5-1024x1024.jpg",
+      artist: "El Train, Marc Mathias",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=51084",
-      color: ["#205950", "#FFFFFF"],
+      color: ["#e5e5ca", "#565034"],
       id: uuidv4(),
       active: false,
     },
@@ -36,69 +86,9 @@ function chillHop() {
       name: "Oasis",
       cover:
         "https://chillhop.com/wp-content/uploads/2022/09/8c2eab8525ace609b0c68b7564eb085d2244ee09-1024x1024.jpg",
-      artist: "Kunal 3",
+      artist: "illiterate, Mr Slipz",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=51080",
-      color: ["#205950", "#FFFFFF"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Shorty",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2023/02/71374d07888a8b1b3fbef53b82d283f53209cc1a-1024x1024.jpg",
-      artist: "Kunal 4",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=51082",
-      color: ["#205950", "#FFFFFF"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Summer Vibe",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2023/02/71374d07888a8b1b3fbef53b82d283f53209cc1a-1024x1024.jpg",
-      artist: "Kunal 5",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=51088",
-      color: ["#205950", "#FFFFFF"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "In the Night",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2023/02/492202568b0d18312224043686735f7f1a47f67b-1024x1024.jpg",
-      artist: "Kunal 6",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=51086",
-      color: ["#205950", "#FFFFFF"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Twilight",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2023/02/71374d07888a8b1b3fbef53b82d283f53209cc1a-1024x1024.jpg",
-      artist: "Kunal 7",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=51084",
-      color: ["#205950", "#FFFFFF"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Oasis",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2022/09/8c2eab8525ace609b0c68b7564eb085d2244ee09-1024x1024.jpg",
-      artist: "Kunal 8",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=51080",
-      color: ["#205950", "#FFFFFF"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Shorty",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2023/02/71374d07888a8b1b3fbef53b82d283f53209cc1a-1024x1024.jpg",
-      artist: "Kunal 9",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=51082",
-      color: ["#205950", "#FFFFFF"],
+      color: ["#aa8f4c", "#214e39"],
       id: uuidv4(),
       active: false,
     },
